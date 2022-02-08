5bd593fb7f207abe18a4f4cb45abc6092f710520
Finley Ge <m13203533462@163.com>
2022-02-08 23:18:22 +0800

    database init

--- server/utility/database/database.go
+++ server/utility/database/database.go
@@ -1,13 +1,29 @@
 package database
 
 import (
+	"fmt"
 	"home-server/config"
+	"log"
+	"os"
 
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func DBInit() {
-	dbHost := config.Config.GetString("data")
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v",
+		config.Config.Database.User,
+		config.Config.Database.Passport,
+		config.Config.Database.Host,
+		config.Config.Database.Port,
+		config.Config.Database.Name)
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln("Database Error!", err)
+		os.Exit(-1)
+	}
+
 }
