c7c6a258b5cd6cacac7fd7fe05910c00bd8a17ef
Finley <m13203533462@163.com>
2022-02-20 22:34:53 +0800

    font problem

--- front-end/public/index.js
+++ front-end/public/index.js
@@ -1,6 +1,6 @@
 var mychar = document.getElementById("identity");
-   mychar.style.color="black";
-   mychar.style.fontSize="20";
-   mychar.style.fontFamily="Microsoft Yahei";
-   mychar.style.fontWeight="bold";
-  
+mychar.style.color = "black";
+mychar.style.fontSize = "20";
+mychar.style.fontFamily = "Microsoft Yahei";
+// ? Maybe this font is not available for linux ... 
+mychar.style.fontWeight = "bold";
