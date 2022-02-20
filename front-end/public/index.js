71523859f0aded6c85eefb08f44ad84d0c0af4be
iaqn <91297802+iaqn@users.noreply.github.com>
2022-02-20 20:31:19 +0800

    unfinished html

---
+++ front-end/public/index.js
@@ -0,0 +1,6 @@
+var mychar = document.getElementById("identity");
+   mychar.style.color="black";
+   mychar.style.fontSize="20";
+   mychar.style.fontFamily="Microsoft Yahei";
+   mychar.style.fontWeight="bold";
+  
