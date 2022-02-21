3f41692868ecb7643eb09b335cd7b97dbd6ef5ef
iaqn <91297802+iaqn@users.noreply.github.com>
2022-02-21 22:50:20 +0800

    Update index.js

--- front-end/public/index.js
+++ front-end/public/index.js
@@ -2,14 +2,6 @@ var mychar = document.getElementById("identity");
 <<<<<<< HEAD
    mychar.style.color="black";
    mychar.style.fontSize="20";
-   //mychar.style.fontFamily="Microsoft Yahei";sans-serif
    mychar.style.fontFamily="arial,sans-serif";
    mychar.style.fontWeight="bold";
-   
-=======
-mychar.style.color = "black";
-mychar.style.fontSize = "20";
-mychar.style.fontFamily = "Microsoft Yahei";
-// ? Maybe this font is not available for linux ... 
-mychar.style.fontWeight = "bold";
 >>>>>>> c7c6a258b5cd6cacac7fd7fe05910c00bd8a17ef
