f8bdc723da38fcdd3e45c950092c2eb72d6cf235
iaqn <91297802+iaqn@users.noreply.github.com>
2022-02-21 22:46:59 +0800

    front refresh

--- front-end/public/index.js
+++ front-end/public/index.js
@@ -1,6 +1,7 @@
 var mychar = document.getElementById("identity");
    mychar.style.color="black";
    mychar.style.fontSize="20";
-   mychar.style.fontFamily="Microsoft Yahei";
+   //mychar.style.fontFamily="Microsoft Yahei";sans-serif
+   mychar.style.fontFamily="arial,sans-serif";
    mychar.style.fontWeight="bold";
-  
+   
